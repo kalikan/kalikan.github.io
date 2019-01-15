ff159933fc32fffc11e6d9b1072a9d6d3a7630fe
kalikan <andrebus79@gmail.com>
2019-01-15 10:12:14 +0900

    Add files via upload

---
+++ count/getCount.php
@@ -0,0 +1,34 @@
+<?php
+// マイサイトソーシャルボタンカウント取得PHP 9サイト 2018.3.7  urbanqee.com                      // 
+
+// *メイン ****************************************
+if (!empty($_GET['countid'])) {
+    $id   = empty($_GET['countid']) ? "" : $_GET['countid'];
+    $res = getVoteCount($id);
+    echo json_encode($res);
+ } else {
+    echo json_encode("syntax error");
+ }
+
+// *関数 ******************************************
+
+function getVoteCount($id){
+ //print "***********************";
+ //カウント数を書きだしてあるファイル名
+ $fileName = "log/" . $id . ".count";
+
+ //fopenでファイルを読み込む (読み込みモード)
+ $fp = @fopen($fileName , "r");
+
+ if($fp){
+ //カウント数書き込み済みのファイルの内容を取得
+ $vote = fgets($fp , 9182);
+ }else{
+ $vote = 0;
+  }
+
+return $vote;
+}
+
+
+?>
